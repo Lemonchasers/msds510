116f4a80571a6dd8074778a9d478b72637acfadd
Chase Lemons <clemons_12@hotmail.com>
2018-07-17 21:30:46 -0500

    Finished problem 4

---
+++ src/msds510/util.py
@@ -0,0 +1,69 @@
+#4
+
+#a
+import datetime
+
+
+numbers = "0123456789"
+
+def get_month(input1):
+    if input1[0] in numbers:
+        month_number = datetime.datetime.strptime(input1, '%d-%b').month
+        print(month_number)
+    else:
+        month_number = datetime.datetime.strptime(input1, '%b-%y').month
+        #print(month_number)
+        return month_number
+#if __name__ == '__main__':
+#                get_month(sys.argv[1])
+
+#print(get_month('Nov-89'))
+
+
+#b
+
+def get_date_joined(input2, input3):
+    yearss = int(input2)
+
+    if input3[0] in numbers:
+        month_numbers = datetime.datetime.strptime(input3, '%d-%b').month
+    else:
+        month_numbers = datetime.datetime.strptime(input3, '%b-%y').month
+
+    day = 1
+
+    date1 = datetime.date(yearss, month_numbers, day)
+    date2 = datetime.datetime.combine(date1,datetime.time())
+    date3 = date2.date()
+    return date3
+    #print(date3)
+
+#print(get_date_joined('1988', 'Jun-88'))
+
+
+#c
+
+def days_since_joined(input4, input5):
+    yearss = int(input4)
+
+    if input5[0] in numbers:
+        month_numbers = datetime.datetime.strptime(input5, '%d-%b').month
+    else:
+        month_numbers = datetime.datetime.strptime(input5, '%b-%y').month
+
+    day = 1
+
+    date1 = datetime.date(yearss, month_numbers, day)
+    date2 = datetime.datetime.combine(date1,datetime.time())
+    date3 = date2.date()
+    today = datetime.date.today()
+
+    result1 = abs(today - date3).days
+    return result1
+    #print(result1)
+
+#print(days_since_joined('1988', 'Jun-88'))
+
+if __name__ == "__main__":
+    import sys
+    util(int(sys.argv[1]))
---
+++ src/test_util_module.py
@@ -0,0 +1,25 @@
+#4d
+
+import sys
+sys.path.insert(0, '/Users/lemonswilliams/Documents/DSC 510/Week 6/msds510/src/msds510/')
+
+import util
+
+
+
+records = [
+                dict(year='1988', intro='Jun-88'),
+                dict(year='1989', intro='May-89'),
+                dict(year='2005', intro='5-May'),
+                dict(year='2013', intro='13-Nov'),
+                dict(year='2014', intro='14-Jan'),
+                ]
+
+for x in records:
+    print("Input Record - ",x)
+    print("Date joined - ", util.get_date_joined(x['year'],x['intro']))
+    print("Days since joined - ", util.days_since_joined(x['year'],x['intro']))
+    print()
+
+
+
